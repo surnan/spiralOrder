b4b77fe3d9c10356771eec2fff3c60cc48f2355b
surnan <suresh_nandlall@outlook.com>
2023-10-24 16:25:44 -0400

    ADD: checkBounds & checkDuplicates verify currentCoordinate should be pushed or turn-right.

--- SpiralMatrix.js
+++ SpiralMatrix.js
@@ -5,10 +5,10 @@ matrix = [
 ]
 
 class CoordinateObject {
-  constructor(value, row, column) {
+  constructor(value, row, col) {
     this.value = value;
     this.row = row;
-    this.col = column;
+    this.col = col;
   }
 }
 
@@ -26,7 +26,6 @@ class DirectionsObject {
     this.moving = ["EAST", "SOUTH", "WEST", "NORTH"]
     this.movingIndex = 0;
   }
-
   getDirection() { return this.moving[this.movingIndex]; }
   turnRight() { this.movingIndex = (this.movingIndex + 1) % 4; }
 }
@@ -40,29 +39,59 @@ answer.push(currentCoordinate);
 function spiralOrder(matrix) {
 
   let temp = currentDirection.getDirection();
-  console.log('ENTERING SWITCH!!!');
-
 
   switch (temp) {
     case 'EAST':
       console.log('going east!!');
+      const checkBounds     = checkCoordBounds(currentCoordinate.row, currentCoordinate.col+1);
+      const checkDuplicate  = checkNoDuplicate(currentCoordinate.row, currentCoordinate.col+1);
+      turnRightOrPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col+1);
       break;
     case 'SOUTH':
       console.log('going south!!');
+      checkCoordBounds(currentCoordinate.row + 1, currentCoordinate.col);
+      checkNoDuplicate(currentCoordinate.row + 1, currentCoordinate.col);
+      turnRightOrPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col+1);
       break;
     case 'WEST':
       console.log('going west!!');
+      checkCoordBounds(currentCoordinate.row, currentCoordinate.col-1);
+      checkNoDuplicate(currentCoordinate.row, currentCoordinate.col-1);
+      turnRightOrPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col+1);
       break;
     case 'NORTH':
       console.log('going north!!');
+      checkCoordBounds(currentCoordinate.row - 1, currentCoordinate.col);
+      checkNoDuplicate(currentCoordinate.row - 1, currentCoordinate.col);
+      turnRightOrPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col+1);
       break;
   }
-
-
-
-
-
 }
 
+function turnRightOrPush(checkBounds, checkDuplicate, row, col){
+  console.log('\n\ncheckBounds', checkBounds);
+  console.log('checkDuplicate', checkDuplicate);
+  console.log('row', row);
+  console.log('col', col);
+}
+
+function checkCoordBounds(newRow, newCol){
+  if ((newRow < boundary.minRow) || (newRow > boundary.maxRow)) {
+    return false;
+  }
+
+  if ((newCol < boundary.minCol) || (newCol > boundary.maxCol)) {
+    return false;
+  }
+  return true;
+}
+
+function checkNoDuplicate(newRow, newCol){
+  let duplicate = false;
+  answer.forEach((ele) => {
+    if ((ele.row === newRow) && (ele.col === newCol)) {duplicate = true;}
+  });
+  return duplicate;
+}
 
 spiralOrder(matrix);
