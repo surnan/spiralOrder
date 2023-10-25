3ac2bcefb8d74923b19c7629b99e9ed37a229f59
surnan <suresh_nandlall@outlook.com>
2023-10-25 01:20:50 -0400

    Project complete.  Code still needs cleanup

--- SpiralMatrix.js
+++ SpiralMatrix.js
@@ -1,8 +1,9 @@
 matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12]
 ]
+
 
 function Coordinate(value, row, col) {
   this.value = value;
@@ -45,33 +46,16 @@ function spiralOrder(matrix) {
   let nextRowCol = path.next();
 
 
-  for (let i = 0; i < 12; i++) {
-    switch (path.getDirection()) {
-      case 'EAST':
-        console.log('EAST!');
-        doSomething(path.next());
-        break;
-      case 'SOUTH':
-        console.log('SOUTH!');
-        doSomething(path.next());
-        break;
-      case 'WEST':
-        console.log('WEST!');
-        doSomething(path.next());
-        break;
-      case 'NORTH':
-        console.log('NORTH!');
-        doSomething(path.next());
-        break;
-    }
-    console.log('i = ' + i + '    answer = ', answer, "\n");
+  for (let i = 0; answer.length < (matrix.length * matrix[0].length); i++) {
+    doSomething(path.next());
   }
+  console.log("ANSWER");
+  console.log(answer);
 }
 
 function doSomething(rowCol) {
   let myChecker = checkBorderLimits(rowCol);
   let noDuplicate = checkAnswerForDuplicate(rowCol);
-  // console.log('myChecker: ', myChecker, '    noDuplicate: ', noDuplicate);
   if (myChecker && noDuplicate) {
     pushAnswerUpdatePath(rowCol);
   } else {
@@ -80,10 +64,8 @@ function doSomething(rowCol) {
 }
 
 function checkAnswerForDuplicate(rowCol) {
-  // console.log ('\n\n\n===========================\n');
   for (let i = 0; i < answer.length - 1; i++) {
-    // console.log('currentRowCol --->  ', currentRowCol, '   answer[i] ---> ', answer[i]);
-    if ((answer[i].row === currentRowCol.row + rowCol.row ) && (answer[i].col === currentRowCol.col + rowCol.col)) {
+    if ((answer[i].row === currentRowCol.row + rowCol.row) && (answer[i].col === currentRowCol.col + rowCol.col)) {
       return false;
     }
   }
@@ -110,141 +92,3 @@ function pushAnswerUpdatePath(rowCol) {
 
   currentRowCol = temp;
 }
-
-
-// function checkCoordBounds(newRow, newCol) {
-//   if ((newRow < boundary.minRow) || (newRow > boundary.maxRow)) {
-//     console.log('row failed checkbounds');
-//     return false;
-//   }
-
-//   if ((newCol < boundary.minCol) || (newCol > boundary.maxCol)) {
-//     console.log('col failed checkbounds');
-//     return false;
-//   }
-//   return true;
-// }
-
-// function checkNoDuplicate(newRow, newCol) {
-//   let duplicate = true;
-//   // console.log("newRow, newCol",newRow, newCol);
-//   // answer.forEach((ele) => {
-//   //   console.log("ele.row, ele.col",ele.row, ele.col);
-//   //   if ((ele.row === newRow) && (ele.col === newCol)) {
-//   //     console.log('duplicate fail');
-//   //     duplicate = false; }
-//   // });
-//   return duplicate;
-// }
-
-// function spiralOrder(matrix) {
-
-//   let temp = currentDirection.getDirection();
-//   let checkBounds;
-//   let checkDuplicate;
-
-//   // console.log("ANSWER B4 FOR-LOOP", answer, "\n");
-//   for (let i = 1; i < 3; i++) {
-//     console.log("\n", i,"----");
-//     console.log("currentCoordinate", currentCoordinate);
-
-//     temp = currentDirection.getDirection();
-
-
-//     switch (temp) {
-//       case 'EAST':
-//         checkBounds     = checkCoordBounds(currentCoordinate.row, currentCoordinate.col + 1);
-//         checkDuplicate  = checkNoDuplicate(currentCoordinate.row, currentCoordinate.col + 1);
-//         if (tryPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col + 1)) {
-//           console.log('EAST - PUSH!!')
-//         } else {
-//           currentDirection.turnRight();
-//           console.log('Turn Right :(');
-//         }
-//         break;
-//       case 'SOUTH':
-//         checkBounds     = checkCoordBounds(currentCoordinate.row + 1, currentCoordinate.col);
-//         checkDuplicate  = checkNoDuplicate(currentCoordinate.row + 1, currentCoordinate.col);
-//         if (tryPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col + 1)) {
-//           console.log('SOUTH - PUSH!!')
-//         } else {
-//           currentDirection.turnRight();
-//           console.log('Turn Right :(');
-//         }
-//         break;
-//       case 'WEST':
-//         checkBounds     = checkCoordBounds(currentCoordinate.row, currentCoordinate.col - 1);
-//         checkDuplicate  = checkNoDuplicate(currentCoordinate.row, currentCoordinate.col - 1);
-//         if (tryPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col + 1)) {
-//           console.log('WEST - PUSH!!')
-//         } else {
-//           currentDirection.turnRight();
-//           console.log('Turn Right :(');
-//         }
-//         break;
-//       case 'NORTH':
-//         checkBounds     = checkCoordBounds(currentCoordinate.row - 1, currentCoordinate.col);
-//         checkDuplicate  = checkNoDuplicate(currentCoordinate.row - 1, currentCoordinate.col);
-//         if (tryPush(checkBounds, checkDuplicate, currentCoordinate.row, currentCoordinate.col + 1)) {
-//           console.log('NORTH - PUSH!!')
-//         } else {
-//           currentDirection.turnRight();
-//           console.log('Turn Right :(');
-//         }
-//         break;
-//     }
-
-//   }
-// }
-
-// function tryPush(checkBounds, checkDuplicate, row, col) {
-//   if (checkBounds && checkDuplicate) {
-//     let temp = new CoordinateObject(matrix[row][col], row % matrix.length, col % matrix[0].length);
-//     answer.push(temp);
-//     console.log('temp', temp);
-//     console.log('answer --> ', answer);
-//     return true
-//   }
-//   // let temp2 = answer[answer.length-1];
-//   // currentCoordinate.value = temp2.value;
-//   // currentCoordinate.row = temp2.row;
-//   // currentCoordinate.col = temp2.col;
-
-//   // console.log("temp2",temp2);
-//   // console.log('answer --> ', answer);
-//   return false;
-// }
-
-
-
-// class CoordinateObject {
-//   constructor(value, row, col) {
-//     this.value = value;
-//     this.row = row;
-//     this.col = col;
-//   }
-// }
-
-// class BoundaryObject {
-//   constructor(matrix) {
-//     this.minRow = 0;
-//     this.minCol = 0;
-//     this.maxCol = matrix[0].length;
-//     this.maxRow = matrix.length;
-//   }
-// }
-
-// class DirectionsObject {
-//   constructor() {
-//     this.moving = ["EAST", "SOUTH", "WEST", "NORTH"]
-//     this.movingIndex = 0;
-//   }
-//   getDirection() { return this.moving[this.movingIndex]; }
-//   turnRight() { this.movingIndex = (this.movingIndex + 1) % this.moving.length; }
-// }
-
-// var answer = [];
-// var boundary = new BoundaryObject(matrix);
-// var currentDirection = new DirectionsObject();
-// var currentCoordinate = new CoordinateObject(matrix[0][0], 0, 0);
-// answer.push(currentCoordinate);
